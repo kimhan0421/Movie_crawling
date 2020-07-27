ba7afa1d5fd3462ada77b82365b62e3975a06192
kimhan0421 <kimhan0421@naver.com>
2020-07-27 16:22:35 +0900

    2. Telegram bot

--- crawling_imax.py
+++ crawling_imax.py
@@ -1,14 +1,17 @@
 import requests
+import telegram
 from bs4 import BeautifulSoup
+from apscheduler.schedulers.blocking import BlockingScheduler
 
-url = 'http://www.cgv.co.kr/common/showtimes/iframeTheater.aspx?areacode=01&theatercode=0013&date=20200724'
-html = requests.get(url)
-soup = BeautifulSoup(html.text, 'html.parser')
-imax = soup.select_one('span.imax')
+bot = telegram.Bot(token = '838890559:AAHO1myIz4tdHgG33wMZwVBgEBHXdkZk8WA')
+url = 'http://www.cgv.co.kr/common/showtimes/iframeTheater.aspx?areacode=01&theatercode=0013&date=20200727'
 
-if(imax):
-    imax = imax.find_parent('div',class_='col-times') #class가 col-times인 것에 하위 부분에 제목 존재
-    title = imax.select_one('div.info-movie > a > strong').text.strip()
-    print(title + '의 IMAX 예매가 열렸습니다.')
-else:
-    print('IMAX를 찾을 수 없습니다.')
+def job_function():
+    html = requests.get(url)
+    soup = BeautifulSoup(html.text, 'html.parser')
+    imax = soup.select_one('span.imax')
+    if(imax):
+        imax = imax.find_parent('div',class_='col-times') #class가 col-times인 것에 하위 부분에 제목 존재
+        title = imax.select_one('div.info-movie > a > strong').text.strip()
+        bot.sendMessage(chat_id= 1303263161, text = title + '의 IMAX 예매가 열렸습니다.')
+        
---
+++ tel.bot.py
@@ -0,0 +1,4 @@
+import telegram
+
+bot = telegram.Bot(token = '838890559:AAHO1myIz4tdHgG33wMZwVBgEBHXdkZk8WA')
+bot.sendMessage(chat_id= 1303263161, text = "테스트 입니다.")
