f79716af6bb6310718fc7f351db71d95a32b349d
kimhan0421 <kimhan0421@naver.com>
2020-07-27 16:23:56 +0900

    2. Telegram bot

--- tel.bot.py
+++ tel.bot.py
@@ -1,4 +1,4 @@
 import telegram
 
-bot = telegram.Bot(token = '838890559:AAHO1myIz4tdHgG33wMZwVBgEBHXdkZk8WA')
+bot = telegram.Bot(token = 'TOKEN')
 bot.sendMessage(chat_id= 1303263161, text = "테스트 입니다.")
